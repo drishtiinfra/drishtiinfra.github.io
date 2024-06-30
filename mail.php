32117fca30a0616b7a755944b5a5e5d836763dff
drishtiinfra <Drishtiinfrastructure2020@gmail.com>
2024-06-30 23:52:43 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "somya123thegreat@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
